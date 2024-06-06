295d32e7b59efce5bfece296e9b04e2774fd356c
MBrandow-IT <masonb@net-deziner.com>
2024-06-06 15:57:13 -0600

    Built navbar AND Left navbar.

--- context/theme.tsx
+++ context/theme.tsx
@@ -13,25 +13,21 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-    if (localTheme) {
-      setMode(localTheme);
-    }
-  }, []);
-
-  useEffect(() => {
     handleThemeChange();
-    localStorage.setItem("theme", mode);
   }, [mode]);
 
   return (
