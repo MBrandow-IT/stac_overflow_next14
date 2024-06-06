8794fb9a8cc060596b15731ae2079d6519d22d68
MBrandow-IT <masonb@net-deziner.com>
2024-06-06 10:56:23 -0600

    Icons and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,47 @@ import {
   // UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = SpaceGrotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const meta: Metadata = {
+  title: "DevFlow",
+  description: "A community of developers helping each other out.",
+  keywords: ["..."],
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary--500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
