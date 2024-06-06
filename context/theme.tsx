03cb69825c105f1cd2f6f60756493a41f8748bc4
MBrandow-IT <masonb@net-deziner.com>
2024-06-06 11:23:09 -0600

    implement theme provider

---
+++ context/theme.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      setMode(localTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleThemeChange();
+    localStorage.setItem("theme", mode);
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return useContext(ThemeContext);
+}
