c53e8ec7690ebd28349ca70cc427fea2d6378935
MBrandow-IT <masonb@net-deziner.com>
2024-06-11 22:25:16 -0600

    Ui enhancements

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -33,7 +33,6 @@ const QuestionCard = ({
   views,
   createdAt,
 }: QuestionCardProps) => {
-  console.log(upVotes);
   return (
     <div className="card-wrapper p-9 sm:px-11 rounded-[10px]">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -28,7 +28,7 @@ const AllAnswers = async ({ questionId, userId }: props) => {
         {result.answers.map((answer: any) => (
           <article className="light-border border-b py-10" key={answer._id}>
             <div className="flex items-center justify-between">
-              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2 w-full">
                 <Link
                   href={`/profile/${answer.author.clerkId}`}
                   className="flex flex-1 items-start gap-1 sm:items-center"
