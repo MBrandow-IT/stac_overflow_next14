90a03ecda61729c137393273ee4e39caafacbaf0
MBrandow-IT <masonb@net-deziner.com>
2024-06-13 16:11:32 -0600

    Implemented URL local search params

--- components/shared/search/LocalSearchBar.tsx
+++ components/shared/search/LocalSearchBar.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 
 interface LocalSearchBarProps {
   route: string;
@@ -17,6 +21,37 @@ const LocalSearchBar = ({
   placeholder,
   otherClasses,
 }: LocalSearchBarProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+
+  const [searchValue, setSearchValue] = React.useState(query || "");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchValue) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: searchValue,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchValue, route, pathname, router, searchParams, query]);
+
   return (
     <div
       className={`flex-1 background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-xl px-4 w-full ${otherClasses}`}
@@ -33,8 +68,9 @@ const LocalSearchBar = ({
       <Input
         type="text"
         placeholder={placeholder}
-        // value=""
+        value={searchValue}
         className="paragraph-regular no-focus placeholder:text-slate-500 background-light800_darkgradient border-none shadow-none outline-none"
+        onChange={(e) => setSearchValue(e.target.value)}
       />
       {iconPosition === "right" && (
         <Image
